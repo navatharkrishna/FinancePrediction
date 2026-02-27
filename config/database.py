736b2f260d2863c0bd54d8a701367c2d8697f818
navatharkrishna <116994749+navatharkrishna@users.noreply.github.com>
2026-02-27 19:18:09 +0530

    Update database.py

--- config/database.py
+++ config/database.py
@@ -3,4 +3,8 @@ from sqlalchemy import create_engine
 
 DATABASE_URL = st.secrets["DATABASE_URL"]
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    pool_recycle=300
+)
