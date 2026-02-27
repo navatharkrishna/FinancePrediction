9fae0fac17db371feeca0961aad05d2074ab8f8b
navatharkrishna <116994749+navatharkrishna@users.noreply.github.com>
2026-02-27 19:14:43 +0530

    Update database.py

--- config/database.py
+++ config/database.py
@@ -1,13 +1,6 @@
+import streamlit as st
 from sqlalchemy import create_engine
-from dotenv import load_dotenv
-import os
 
-load_dotenv()
+DATABASE_URL = st.secrets["DATABASE_URL"]
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-engine = create_engine(
-    DATABASE_URL,
-    pool_size=5,
-    max_overflow=10
-)
+engine = create_engine(DATABASE_URL)
